593b9e21490b464ed1751387a627b4f3616aa01b
nitishkumaram <NITISH@192.168.3.114>
2017-12-12 23:49:43 +0530

    Commited Prgm2

---
+++ src/Prgm2.java
@@ -0,0 +1,26 @@
+/* Task 
+In this challenge, you must read  integers from stdin and then print them to stdout. 
+Each integer must be printed on a new line. To make the problem a little easier, a 
+portion of the code is provided for you in the editor below.*/
+
+
+import java.util.*;
+
+public class Prgm2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // Complete this line
+        int b = scan.nextInt();
+        // Complete this line
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        // Complete this line
+        System.out.println(b);
+        // Complete this line
+        System.out.println(c);
+    }
+}
+
