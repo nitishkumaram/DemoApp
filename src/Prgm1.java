9e91f15a0558fce9ece244c81b75b33695468d13
nitishkumaram <nitishkumar@outlook.com>
2018-09-02 00:28:58 +0530

    Modified "Hello Java" to "Demo App"

--- src/Prgm1.java
+++ src/Prgm1.java
@@ -10,6 +10,6 @@ public class Prgm1 {
     public static void main(String[] args) {
         /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
         System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
+        System.out.println("Demo App");
     }
 }
