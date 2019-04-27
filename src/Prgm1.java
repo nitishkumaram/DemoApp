ae1fc1b17ac82262d034d2695074d90a87f7a6da
nitishkumaram <nitishkumar@outlook.com>
2019-04-27 22:56:40 +0530

    commented skipDefaultCheckout

--- src/Prgm1.java
+++ src/Prgm1.java
@@ -9,7 +9,7 @@ public class Prgm1 {
 
     public static void main(String[] args) {
         /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
-        System.out.println("Hello, World.");
+        System.out.println("Commented skipDefaultCheckout");
         System.out.println("Demo App");
     }
 }
